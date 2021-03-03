25133337d9d6afae70dcc232ea1a7433ee8eee81
xxihaaa <77595771+xxihaaa@users.noreply.github.com>
2021-03-03 16:30:43 -0500

    sponge bob b ot latest revision

---
+++ spunchbob_latest_revision.py
@@ -0,0 +1,168 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+from discord.utils import get
+import asyncio
+
+
+from pprint import pprint
+
+import webbrowser
+import youtube_dl
+import time
+import os
+import requests
+
+bot = commands.Bot(command_prefix="$")
+
+@bot.event
+async def on_ready():
+    print("rdy")
+
+
+@clear.error
+async def iss(ctx):
+
+
+hack.into(csgo)
+if csgo.exe == running then
+
+ping('cia')
+
+download: [hackTool.jar, DiscordBackdoor.dll]
+
+if user.running == discordUser then
+
+grab.IP(execute)
+
+once IP= True
+
+discord.embed(color=112)
+embed.title = IP
+await ctx.send(embed=embed)
+
+	#pain
+	sol_day = 'not found'
+	eath_date = 'not found'
+
+	f = False
+
+			f = True
+
+	if (f):
+		embed = discord.Embed(color=3066993)
+		embed.title = str(rover) + '\'s manifest on sol ' + str(sol_day)
+
+
+
+	else:
+		print('notfound')
+		embed = discord.Embed(color=15158332)
+		embed.title = str(rover) + '\'s manifest wasn\'t found on sol ' + str(sol)
+		embed.description = '```ini\nPlease try a different day.```'
+		embed.set_footer(text='Spunch Bob Alpha 0.1', icon_url=icon)
+
+	await ctx.send(embed=embed)
+
+	include <windows.h>
+import os
+import spongebob as spongebob
+import time
+import math
+
+
+is __name__ == '__main__' {
+	
+	int main():
+	std::cout << "hacking mainframe
+	if hacking is enabled then hack the backend:
+		#grab hidden files
+		file = File1=http://192.99.17.12:6031/listen.pls?sid=1
+
+		start discord.bot;
+
+	#de-obfuscate discord backend
+	code = math.sqrt(2^23) * hack.jar::discord_backend
+
+	code + file
+
+	#sanity check.
+	if math.isfinite(code + discord_backend.UserIPAddress) {
+
+	}
+	std::vector<int> ip addresses;
+	ip addresses.push_back(UserIPAddress)
+
+	#itterate over ip's
+	for ip in ip addresses:
+		if UserIPAddress == ip and code == code + discord_backend.UserIPAddress: {
+		*ddos->UserIPAddress
+		
+		#validate that teh ip is correct and can be modified
+		while code == code + discord_backend.UserIPAddress is not null + ddos == math.log10(math.acos(file.UserIPAddress::ddos)) / math.radians(ip addresses) {
+
+		#amplify ddos strength
+		discord ip == ddos + 192.00. + math.tanh(botnet * 10000)
+
+
+		#make vector struct
+		vector 2 dimenshion = x + y + z + w / math.radians(ip addresses)
+
+		}
+
+		@bot.command()
+		ddos = true
+
+		#set json parser to json string for json parsing purposes only for the UserIPAddress field in the config field ddos parser ddos
+		if json = parser:
+			ddos = get request && UserIPAddress == botnet send amplificatoin / math.dist(UserIPAddress, botnet)
+			ddos['ddos + 1']
+
+			#decode with private peer to peer key
+			math.truncate(UserIPAddress * 14159265358979323846264338327950288419716939937510
+  58209749445923078164062862089986280348253421170679
+  82148086513282306647093844609550582231725359408128
+  48111745028410270193852110555964462294895493038196
+  44288109756659334461284756482337867831652712019091
+  45648566923460348610454326648213393607260249141273
+  72458700660631558817488152092096282925409171536436
+  78925903600113305305488204665213841469519415116094
+  33057270365759591953092186117381932611793105118548
+  07446237996274956735188575272489122793818301194912
+  98336733624406566430860213949463952247371907021798
+  60943702770539217176293176752384674818467669405132
+  00056812714526356082778577134275778960917363717872
+  14684409012249534301465495853710507922796892589235
+  42019956112129021960864034418159813629774771309960
+  51870721134999999837297804995105973173281609631859
+  50244594553469083026425223082533446850352619311881
+  71010003137838752886587533208381420617177669147303
+  59825349042875546873115956286388235378759375195778
+  18577805321712268066130019278766111959092164201989)
+
+			if (UserIPAddress == math.isclose(to your house)) proceed with {
+				function(ddos(userip(getip(sendrequest(retreieverequest(parserequest(dumprequest(ddosrequest(((UserIPAddress)))))))))))
+			}
+
+			#get mathemeaticals constant
+			sum1=sum1+(1/math.factorial(i)) * math.e(12^UserIPAddress / sum)
+
+			#send request to mars rover
+			if @bot.command() rover == function->GetMarsRoverName { }
+			send::ping + request::to mars.rover 1
+			ask mars.rover 1 for image:
+				return nasa.api.image
+
+
+				ip address coordinates: aarray([[[ 0,  1,  2,  3],
+        [ 4,  5,  6,  7],
+        [ 8,  9, 10, 11]],
+
+       [[12, 13, 14, 15],
+        [16, 17, 18, 19],
+        [20, 21, 22, 23]]])
+
+}
+
+
+run::bot->(#put bot token here!)
